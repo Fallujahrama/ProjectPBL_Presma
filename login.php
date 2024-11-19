3df3ec4139d56e443cb3de1c9e05405225b5fbde
Fallujahrama <fallujahrc@gmail.com>
2024-11-19 15:43:23 +0700

    coba login figma

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GoPres Polinema</title>
+    <link rel="stylesheet" href="styleLogin.css">
+</head>
+<body>
+    <div class="container">
+        <img class="background-image" src="assets/backgroundImage.jpg" />
+        <div class="overlay"></div>
+        <div class="welcome-text">
+            Selamat datang di <br/>GoPres Polinema!
+        </div>
+        <div class="subtitle">
+            Pusat Prestasi Mahasiswa
+        </div>
+        <div class="department">
+            Jurusan Teknologi Informasi
+        </div>
+        <img class="logo" src="assets/logoPoltek.jpg" />
+        
+        <!-- Input box for email with icon -->
+        <div class="input-box first">
+            <div class="icon">
+                <img src="assets/Profile.jpg" alt="user-icon">
+            </div>
+            <input type="text" placeholder="Email" class="input-field">
+        </div>
+        
+        <!-- Input box for password with icon -->
+        <div class="input-box second">
+            <div class="icon">
+                <img src="assets/Key.jpg" alt="key-icon">
+            </div>
+            <input type="password" placeholder="Password" class="input-field">
+        </div>
+        
+        <!-- Login button with icon -->
+        <div class="button login-button">
+            <div class="button-text">Login</div>
+            <div class="icon login-icon">
+                <img src="assets/Login.jpg" alt="login-icon">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
